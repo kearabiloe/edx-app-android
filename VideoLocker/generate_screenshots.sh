3177e45f325ea94cc4b8017805a564a2e46de616
Kearabiloe <kea@Proversity.org>
2016-02-29 14:58:14 +0200

    Create avd and launch emulator

---
+++ VideoLocker/generate_screenshots.sh
@@ -0,0 +1,21 @@
+echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
+adb install -r build/outputs/apk/VideoLocker-prod-release.apk
+AppPackage=<package>
+ScreenshotsDir=src/main/play/en-GB/listing/phoneScreenshots
+adb shell am start -n $AppPackage/.view.LoginActivity -W
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ScreenshotsDir/LoginActivity.png
+adb shell am force-stop $AppPackage
+adb shell am start -n $AppPackage/.view.RegisterActivity -W
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ScreenshotsDir/RegisterActivity.png
+adb shell am force-stop $AppPackage
+adb shell am start -n $AppPackage/.view.SplashActivity -W
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ScreenshotsDir/SplashActivity.png
+adb shell am force-stop $AppPackage
+adb shell am start -n $AppPackage/.view.LaunchActivity -W
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ScreenshotsDir/LaunchActivity.png
+adb shell am force-stop $AppPackage
+adb shell am start -n $AppPackage/.view.MyCoursesListActivity -W
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ScreenshotsDir/MyCoursesListActivity.png
