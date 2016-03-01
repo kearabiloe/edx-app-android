4e7427e69b4a84027c8db8530ec28f9a2388a4dd
Kearabiloe <kea@proversity.org>
2016-03-01 10:54:46 +0200

    Update generate_screenshots.sh

--- VideoLocker/generate_screenshots.sh
+++ VideoLocker/generate_screenshots.sh
@@ -1,5 +1,5 @@
 echo "Generating screenshots..."
-echo no | android create avd --force -n test -t android-23 --abi default
+echo no | android create avd --force -n test -t android-23 --abi default/armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
