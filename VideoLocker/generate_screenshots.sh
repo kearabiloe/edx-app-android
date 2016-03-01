293cb6315e5184c5491e15e2cb53e5e59508d634
Kearabiloe <kea@proversity.org>
2016-03-01 14:48:31 +0200

    Update generate_screenshots.sh

--- VideoLocker/generate_screenshots.sh
+++ VideoLocker/generate_screenshots.sh
@@ -2,7 +2,8 @@ echo "Generating screenshots..."
 echo "y" | android update sdk -a --no-ui --filter sys-img-armeabi-v7a-android-23,sys-img-x86_64-android-23
 echo no | android create avd --force -n test -t android-23 --abi default/armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
-android-wait-for-emulator
+adb wait-for-device
+#android-wait-for-emulator
 adb shell input keyevent 82 &
 adb install -r build/outputs/apk/VideoLocker-prod-release.apk
 AppPackage=org.edx.mobile
